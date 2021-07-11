bb138be1a5e3b6898bb91acf1d713d86ee6c2809
guilaumedeparis <deparis.guillaume@gmail.com>
2021-07-11 20:31:15 +0200

    [*] Modificaiton de script

--- ConstructRoad.gd
+++ ConstructRoad.gd
@@ -6,23 +6,31 @@ extends Node2D
 # var b = "text"
 onready var GroundMap = $GroundMap
 onready var RoadMap = $RoadMap
-onready var BuildMap = $BuildMap
 
-const N = 1
-const E = 2
-const S = 4
-const W = 8
-
-var cell_road = {Vector2(0, -1): N, Vector2(1, 0): E, Vector2(0, 1): S, Vector2(-1, 0): W}
 var dragging = false
-var path = []
+
 
 var startTile: Vector2
 var endTile: Vector2
-var arrayRoad = []
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	initMap()
 	pass # Replace with function body.
+
+
+var distance = []
+var mapHeight = 10
+var mapWidth = 10
+const TILE_WALL = 16
+
+const West = {"x": 0, "y": 1}
+const North = {"x": -1, "y": 0}
+const South = {"x": 1, "y": 0}
+const East = {"x": 0, "y": -1}
+const Southwest = {"x": 1, "y": 1}
+const Southeast = {"x": 1, "y": -1}
+const Northwest = {"x": -1, "y": 1}
+const Noarteast = {"x": -1, "y": -1}
 
 func _input(event):
 	var pos = get_global_mouse_position()
@@ -32,83 +40,23 @@ func _input(event):
 	elif event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
 		if not dragging and event.pressed:
 			dragging = true
-#			obtenir la liste de tuile permettant d'accéder de la tuile de départ à la tuile sous la souris.
-#			construct_road(GroundMap.world_to_map(pos))
 			startTile = GroundMap.world_to_map(pos)
 			endTile = GroundMap.world_to_map(pos)
-			getPath(startTile, endTile)			
+			getDistanceStartToEnd(startTile, endTile)
 		if dragging and not event.pressed:
 			dragging = false
-			
 	elif event is InputEventMouseMotion and dragging:
-#		clearPath()
-		if !arrayRoad.has(GroundMap.world_to_map(pos)):
-			endTile = GroundMap.world_to_map(pos)
-			getPath(startTile, endTile)
-#		construct_road(GroundMap.world_to_map(pos))
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-func clearPath():
-	print("arrayRoad: ", arrayRoad)
-	for road in arrayRoad:
-		RoadMap.set_cellv(road, -1)
-	RoadMap.update()
+		endTile = GroundMap.world_to_map(pos)
 
-func getPath(startTile, endTile):
-#	print("startTile: ", startTile)
-#	print("endTile: ", endTile)
-	for tilex in endTile.x+1 - startTile.x:
-		if !arrayRoad.has(Vector2(tilex, startTile.y)):
-			arrayRoad.push_back(Vector2(tilex, startTile.y))
-	for tiley in endTile.y+1 - startTile.y:
-		if !arrayRoad.has(Vector2(endTile.x, tiley)):
-			arrayRoad.push_back(Vector2(endTile.x, tiley))
-
-	for road in arrayRoad:
-		RoadMap.set_cellv(road, 11)
-		update_neighbor(road)
-
-func check_neighbors(cell):
-	var weightList = {}
-	for n in cell_road.keys():
-		var neighbor = cell + n
-		var currentNeighbor_weight = RoadMap.get_cellv(neighbor)
-		weightList[cell_road[n]] = currentNeighbor_weight	
-	return weightList
-
-
-#Pas très propre, revoir la gestion des multiples 16
-func getRoadDirection(array):
-	var dir = 15
-	var countNeg1 = 0
-	var count = 0
-	var count16 = false
-	if array.values().find_last(16) >= 1: #Au moin 2 occurrence de 16
-		count16 = true
-	for a in array.keys():
-		if array.get(a) == 16: #récupération du degré de liberté
-			count = count + 1
-			if count16 && count < 2:
-				dir = dir - a
-		elif array.get(a) != -1: # on récupère le degré de liberté, cependant la cellule adjacente devra être mise à jour.
-			dir = dir - a
-		else:
-			countNeg1 = countNeg1 + 1
-	if countNeg1 == 4:
-		dir = 11 # route par défaut.
-	return dir
+var tileList = []
+var Neighbor = load("Neighbor.gd")
+var _Node = load("PathFinding_node.gd")
+func initMap():
+	#pour chaque tile, créer une instance de PathFinding_Node
+	for tile in GroundMap.get_used_cells():
+		tileList.push_back(_Node.new(tile.x, tile.y, true))
+		
 	
-func update_neighbor(cell):
-	var array = []
-	for n in cell_road.keys():
-		array.push_back(Vector2(cell.x + n.x, cell.y + n.y))
-		for neighbor in array:
-			if RoadMap.get_cellv(neighbor) != -1:
-				var weight = check_neighbors(neighbor)
-				RoadMap.set_cellv(neighbor, getRoadDirection(weight))
-
-func construct_road(tilePost:Vector2):
-	var weight = check_neighbors(tilePost)
-	RoadMap.set_cellv(tilePost, getRoadDirection(weight))
-	update_neighbor(tilePost)
+func getDistanceStartToEnd(startPos, endPos):
+	distance[startPos.x][startPos.y] = Neighbor.new()
+	print(distance[startPos.x][startPos.y][West])
