100804ef21f0ef6240b9d5279b100b9b54ea089b
guilaumedeparis <deparis.guillaume@gmail.com>
2021-07-11 22:07:53 +0200

    [*] Finalisation AStar

--- TestAStar.gd
+++ TestAStar.gd
@@ -9,16 +9,22 @@ onready var RoadMap = $RoadMap
 var _AStar = load("_AStar.gd")
 var _Node = load("_Node.gd")
 var dragging = false
+var leftButtonPressed = false
 var startNode:_Node
 var endNode:_Node
 var path:Array
+var groundMapArray:Array
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	GroundMap.get_canvas_item()
+	for tileList in GroundMap.get_used_cells():
+		groundMapArray.push_back(_Node.new(tileList.x, tileList.y, GroundMap.get_cellv(tileList)))
 	pass
 
 func _input(event):
 	var pos = get_global_mouse_position()
+	
 	if event is InputEventMouseButton and event.button_index == BUTTON_RIGHT and event.pressed:
 			var tile = GroundMap.world_to_map(pos)
 			GroundMap.set_cellv(tile, -1)
@@ -26,39 +32,40 @@ func _input(event):
 		var tile = GroundMap.world_to_map(pos)
 		startNode = _Node.new(tile.x, tile.y, GroundMap.get_cellv(tile))
 		if not dragging and event.pressed:
+			leftButtonPressed = true
 			dragging = true
 		if dragging and not event.pressed:
+			leftButtonPressed = false
 			dragging = false
-	elif event is InputEventMouseMotion and dragging:
+			path.clear()
+	elif event is InputEventMouseMotion and leftButtonPressed:
+		if dragging:
+			clearTempPath()
 		var tile = GroundMap.world_to_map(pos)
 		endNode = _Node.new(tile.x, tile.y, GroundMap.get_cellv(tile))
-		clearPath()
 		pathFinding_AStar()
 		drawPath()
 func drawPath():
 	for a in path:
 		print(a.x, a.y)
 		RoadMap.set_cell(a.x, a.y, 2)
-func clearPath():
+func clearTempPath():
 	for a in path:
 		RoadMap.set_cell(a.x, a.y, -1)
 
 func pathFinding_AStar():
 	var groundMap:Array
-	var newEndNode = _Node.new(startNode.x, endNode.y)
-	var newStartNode = _Node.new(endNode.x, startNode.y)
-	endNode = _Node.new(10, 10)
-	startNode = _Node.new(0, 0)
-#	print("(" , startNode.x,  " - ", startNode.y, "), (", endNode.x, " - ", endNode.y,")")
-#	print("(", newStartNode.x,  " - ", newStartNode.y,"), (", newEndNode.x, " - ", newEndNode.y,")")
-	for nodeX in (newEndNode.x - newStartNode.x)+1:
-		for nodeY in (newEndNode.y - newStartNode.y)+1:
-			groundMap.push_back(_Node.new(nodeX+newStartNode.x, nodeY+newStartNode.y, GroundMap.get_cell(nodeX, nodeY)))
-#	print("=== Debut ===")
-#	for a in groundMap:
-#		print(a.x, " - ", a.y)
-#	print("=== Fin ===")
-	var astar = _AStar.new(startNode, endNode, groundMap)
+#	startNode = _Node.new(0, 0)
+#	endNode = _Node.new(6, 7)
+	var newEndNode
+	var newStartNode
+	if startNode.x > endNode.x:
+		newEndNode = _Node.new(startNode.x, endNode.y)
+		newStartNode = _Node.new(endNode.x, startNode.y)
+	else:
+		newEndNode = endNode
+		newStartNode = startNode
+	var astar = _AStar.new(startNode, endNode, groundMapArray)
 	path = astar.aStar()
 	
 
